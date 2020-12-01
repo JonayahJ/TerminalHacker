c0c0b23e5958b01ec0ce4ea7c39dd5f6596b6206
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-12-01 15:48:51 -0500

    set up menu input options

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -7,16 +7,13 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ShowMainMenu();
+        ShowMainMenu("Hello, Jo.");
     }
 
-    void ShowMainMenu()
+    void ShowMainMenu(string greeting)
         {
             Terminal.ClearScreen();
-
-            string greeting = "Hello, Jo.";
             Terminal.WriteLine(greeting);
-            
             Terminal.WriteLine("What would you like to hack into?");
             Terminal.WriteLine("Press 1 for City Hall.");
             Terminal.WriteLine("Press 2 for the Police Precint.");
@@ -25,6 +22,32 @@ public class Hacker : MonoBehaviour
         }
 
     // Update is called once per frame
+
+    void OnUserInput(string input)
+	{
+		print("The user typed: \"" + input + "\"");
+        if(input == "1")
+        {
+            print("You chose City Hall.");
+        }
+        else if(input == "2")
+        {
+            print("You chose the Police Precint.");
+        }
+        else if(input == "3")
+        {
+            print("You choose the Hospital.");
+        }
+        else if(input == "Menu")
+        {
+            ShowMainMenu("Resetting options...");
+        }
+        else
+        {
+            print(false);
+        }
+	}
+
     void Update()
     {
         
