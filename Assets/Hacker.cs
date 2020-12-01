be7abf62df90d201547d447dbee299e41215da6b
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-12-01 15:16:13 -0500

    added showmainmenu fx

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -7,12 +7,24 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello console");
+        ShowMainMenu();
     }
+    
+    void ShowMainMenu()
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("What would you like to hack into?");
+            Terminal.WriteLine("Press 1 for City Hall.");
+            Terminal.WriteLine("Press 2 for the Police Precint.");
+            Terminal.WriteLine("Press 3 for the Hospital.");
+            Terminal.WriteLine("Enter your selection:");
+        }
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    
 }
