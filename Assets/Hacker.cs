73a53338cac5366d4b1559e6678854e00ee90978
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-12-01 16:22:02 -0500

    adding the setup for states

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game State
+    int level;
+    
     // Start is called before the first frame update
     void Start()
     {
-        ShowMainMenu("Hello, Jo.");
+        ShowMainMenu("Hello, Hacker.");
     }
 
     void ShowMainMenu(string greeting)
@@ -15,38 +18,49 @@ public class Hacker : MonoBehaviour
             Terminal.ClearScreen();
             Terminal.WriteLine(greeting);
             Terminal.WriteLine("What would you like to hack into?");
-            Terminal.WriteLine("Press 1 for City Hall.");
+            Terminal.WriteLine("Press 1 for Hospital.");
             Terminal.WriteLine("Press 2 for the Police Precint.");
-            Terminal.WriteLine("Press 3 for the Hospital.");
+            Terminal.WriteLine("Press 3 for the City Hall.");
             Terminal.WriteLine("Enter your selection:");
         }
 
     // Update is called once per frame
-
     void OnUserInput(string input)
 	{
 		print("The user typed: \"" + input + "\"");
-        if(input == "1")
-        {
-            print("You chose City Hall.");
-        }
-        else if(input == "2")
-        {
-            print("You chose the Police Precint.");
-        }
-        else if(input == "3")
-        {
-            print("You choose the Hospital.");
-        }
-        else if(input == "Menu")
+        if(input == "menu")
         {
             ShowMainMenu("Resetting options...");
         }
+        else if(input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if(input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if(input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else if(input == "Hack the planet!")
+        {
+            Terminal.WriteLine("Nice to see you, ZeroCool!  Please make your selection.");
+        }
         else
         {
-            print(false);
+            Terminal.WriteLine("Please select a valid level.");
         }
 	}
+
+    void StartGame()
+    {
+        Terminal.WriteLine("You chose Level " + level);
+    }
 
     void Update()
     {
